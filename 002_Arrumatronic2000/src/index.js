71751f7f7d43dfff75faa7336d36d6afcd2ff873
GustavoTreccia <g.treccia@outlook.com>
2023-05-16 21:29:58 -0300

    002 abre navegador, acessa whatsapp e acessa o anotações

--- 002_Arrumatronic2000/src/index.js
+++ 002_Arrumatronic2000/src/index.js
@@ -1,12 +1,13 @@
-const puppeteer = require('puppeteer');
+const { chromium } = require("playwright");
 //const cron = require('node-cron');
 
 const horarioDeEnvio = '0 16 * * *';
 
-const caminhoDaFigurinha = 'C:/Users/gtrec/Desktop/botWorkshop/002_Arrumatronic2000/src/figurinha.png';
+const caminhoDaFigurinha = 'C:\\Users\\gtrec\\Desktop\\botWorkshop\\002_Arrumatronic2000\\src\\figurinha.png';
 
 async function lancaFigurinha() {
-    const browser = await puppeteer.launch({ headless: false });
+    console.log("Entrou na funnção");
+    const browser = await chromium.launch({ executablePath : 'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe', headless : false, inprivate: false});
     const page = await browser.newPage();
     await page.goto('https://web.whatsapp.com');
     
